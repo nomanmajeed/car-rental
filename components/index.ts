b7363235442f0f544e42e64769f678483b3c014c
nomanmajeed <nomanmajeed986@gmail.com>
2023-08-13 15:35:16 +0500

    feat: car details model section complete

--- components/index.ts
+++ components/index.ts
@@ -4,8 +4,10 @@ import Navbar from "./Navbar"
 import Footer from "./Footer"
 import SearchBar from "./SearchBar"
 import CustomFilter from "./CustomFilter"
+import CarCard from "./CarCard";
+
 
 
 export { 
-    Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter
+    Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, CarCard
 }
